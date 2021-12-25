387fb1a91a8258f1e7ad8d6efa442386250c373a
Lamija Mehic <lamijamehic1303@gmail.com>
2021-12-25 23:25:07 +0100

    Output changed

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-#Prvi_Projekat
-Ovo je moj prvi projekat.
+#firstProject
+This is my first project
 
-To je WiFi extractor koji preko cmd komandi ispisuju
-nazive i passworde WiFi mreža na koje je računar spojen.
+It is basically a Wi-fi extractor and it's main purpose is to
+list the names and passwords of Wi-fi connections on the current 
+PC wile interacting with the cmd.
--- wifiExtractor.py
+++ wifiExtractor.py
@@ -6,7 +6,7 @@ for i in profiles:
     results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
     results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
     try:
-        print ("{:<30}|  {:<}".format(i, results[0]))
+        print ("Name:{:<5}|  Password:{:<}".format(i, results[0]))
     except IndexError:
-        print ("{:<30}|  {:<}".format(i, ""))
+        print ("{:<15}|  {:<}".format(i, ""))
 input("")
